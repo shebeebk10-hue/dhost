484bfb4ed5260c5728e49ef8eb5830d1f1dcdb9f
shebeebk10-hue <shebeebk10@gmail.com>
2026-05-18 12:41:51 +0530

    Added Django admin

--- DBApp/admin.py
+++ DBApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Shop, Invoice
+
+admin.site.register(Shop)
+admin.site.register(Invoice)
